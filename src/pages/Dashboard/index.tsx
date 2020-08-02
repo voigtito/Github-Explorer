8eaf6d0fcc1f0378a0f93830d11c9f31ed6c070d
Gustavo Voigt <voigt.gustavoh@gmail.com>
2020-08-02 11:45:08 -0300

    Repositories visual without API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -73,7 +73,7 @@ const Dashboard: React.FC = () => {
 
         <Repositories>
             {repositories.map(repository => (
-            <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
+                <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
                     <img
                         src={repository.owner.avatar_url}
                         alt={repository.owner.login}
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './style';
+import LogoImg from '../../assets/Logo.svg';
 
 interface RepositoryParams {
     repository: string;
@@ -10,10 +14,50 @@ const Repository: React.FC = () => {
 
     const { params } = useRouteMatch<RepositoryParams>();
 
-    return (
-    <h1>
-        Repository:{params.repository}
-    </h1>
+    return (<>
+        <Header>
+            <img src={LogoImg} alt="Github Explorer" />
+            <Link to="/dashboard">
+                <FiChevronLeft size={16} />
+            Voltar
+            </Link>
+        </Header>
+        <RepositoryInfo>
+            <header>
+                <img src="https://avatars1.githubusercontent.com/u/55980259?s=460&u=14290cea59e995dc4544857f4f6e698c9bcc4ec5&v=4" alt="rocketseat" />
+                <div>
+                    <strong>rocketseat/unform</strong>
+                    <p>descrição do repo</p>
+                </div>
+            </header>
+            <ul>
+                <li>
+                    <strong>1080</strong>
+                    <span>Stars</span>
+                </li>
+                <li>
+                    <strong>48</strong>
+                    <span>Forks</span>
+                </li>
+                <li>
+                    <strong>67</strong>
+                    <span>Issues abertas</span>
+                </li>
+            </ul>
+        </RepositoryInfo>
+
+        <Issues>
+            <Link
+                to={`/repositories/`
+                }>
+                <div>
+                    <strong>as</strong>
+                    <p>assa</p>
+                </div>
+                <FiChevronRight size={20} />
+            </Link>
+        </Issues>
+    </>
     );
 }
 
