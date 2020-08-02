42d72248bc443ee35dfa93f18dc93cc1244fbe45
Gustavo Voigt <voigt.gustavoh@gmail.com>
2020-08-02 12:21:12 -0300

    Issues API

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import { Header, RepositoryInfo, Issues } from './style';
 import LogoImg from '../../assets/Logo.svg';
@@ -9,53 +10,101 @@ interface RepositoryParams {
     repository: string;
 }
 
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+}
+
+interface Issue {
+    title: string;
+    id: number;
+    user: {
+        login: string;
+    }
+    html_url: string;
+}
+
 // React.FC = React Functional component
 const Repository: React.FC = () => {
 
+    const [repository, setRepository] = useState<Repository | null>(null);
+    const [issues, setIssues] = useState<Issue[]>([]);
+
     const { params } = useRouteMatch<RepositoryParams>();
+
+    useEffect(() => {
+        api.get(`repos/${params.repository}`).then(response => {
+            setRepository(response.data);
+        });
+
+        api.get(`repos/${params.repository}/issues`).then(response => {
+            setIssues(response.data);
+        });
+
+        // async function loadData(): Promise<void> {
+        //     const [repository, issues] = await Promise.all([
+        //         api.get(`repos/${params.repository}`),
+        //         api.get(`repos/${params.repository}/issues`)
+        //     ])
+        // }
+
+        // loadData();
+
+    }, [params.repository]);
 
     return (<>
         <Header>
             <img src={LogoImg} alt="Github Explorer" />
-            <Link to="/dashboard">
+            <Link to="/">
                 <FiChevronLeft size={16} />
-            Voltar
+                Voltar
             </Link>
         </Header>
-        <RepositoryInfo>
-            <header>
-                <img src="https://avatars1.githubusercontent.com/u/55980259?s=460&u=14290cea59e995dc4544857f4f6e698c9bcc4ec5&v=4" alt="rocketseat" />
-                <div>
-                    <strong>rocketseat/unform</strong>
-                    <p>descrição do repo</p>
-                </div>
-            </header>
-            <ul>
-                <li>
-                    <strong>1080</strong>
-                    <span>Stars</span>
-                </li>
-                <li>
-                    <strong>48</strong>
-                    <span>Forks</span>
-                </li>
-                <li>
-                    <strong>67</strong>
-                    <span>Issues abertas</span>
-                </li>
-            </ul>
-        </RepositoryInfo>
+        {repository && (
+            <RepositoryInfo>
+                <header>
+                    <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+                    <div>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>{repository.stargazers_count}</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>{repository.forks_count}</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>{repository.open_issues_count}</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>)}
 
         <Issues>
-            <Link
-                to={`/repositories/`
-                }>
-                <div>
-                    <strong>as</strong>
-                    <p>assa</p>
-                </div>
-                <FiChevronRight size={20} />
-            </Link>
+            {issues.map(issue =>
+                <a
+                    key={issue.id}
+                    href={issue.html_url}
+                >
+                    <div>
+                        <strong>{issue.title}</strong>
+                        <p>{issue.user.login}</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            )}
         </Issues>
     </>
     );
