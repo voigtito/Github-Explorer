e39ef64fda9ddfd8bb40d49e0602e51078425774
Gustavo Voigt <voigt.gustavoh@gmail.com>
2020-08-02 10:32:53 -0300

    Connecting with github API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,65 +1,64 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import LogoImg from '../../assets/Logo.svg';
+import api from '../../services/api';
 
 import { Title, Form, Repositories } from './style';
 
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+
+}
+
 // React.FC = React Functional component
 const Dashboard: React.FC = () => {
+
+    const [newRepo, setNewRepo] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+        event.preventDefault();
+
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+
+        const repository = response.data;
+
+        setRepositories([...repositories, repository]);
+        setNewRepo('');
+    }
+
     return (<>
         <img src={LogoImg} alt="Github Explorer logo" />
         <Title>Explore Repositórios do GitHub</Title>
 
-        <Form>
-            <input placeholder="Digite o nome do repositório" />
+        <Form onSubmit={handleAddRepository}>
+            <input
+                value={newRepo}
+                onChange={(e) => setNewRepo(e.target.value)}
+                placeholder="Digite o nome do repositório"
+            />
             <button type="submit" >Pesquisar</button>
         </Form>
 
         <Repositories>
-            <a href="teste">
-                <img
-                    src="https://avatars1.githubusercontent.com/u/55980259?s=460&u=14290cea59e995dc4544857f4f6e698c9bcc4ec5&v=4"
-                    alt="Gustavo Voigt"
-                />
-                <div>
-                    <strong>rocketseat/unform</strong>
-                    <p>Easy peasy highly scalable REactJS & React Native Forms! </p>
-                </div>
-                <FiChevronRight size={20}/>
-            </a>
-            <a href="teste">
-                <img
-                    src="https://avatars1.githubusercontent.com/u/55980259?s=460&u=14290cea59e995dc4544857f4f6e698c9bcc4ec5&v=4"
-                    alt="Gustavo Voigt"
-                />
-                <div>
-                    <strong>rocketseat/unform</strong>
-                    <p>Easy peasy highly scalable REactJS & React Native Forms! </p>
-                </div>
-                <FiChevronRight size={20}/>
-            </a>
-            <a href="teste">
-                <img
-                    src="https://avatars1.githubusercontent.com/u/55980259?s=460&u=14290cea59e995dc4544857f4f6e698c9bcc4ec5&v=4"
-                    alt="Gustavo Voigt"
-                />
-                <div>
-                    <strong>rocketseat/unform</strong>
-                    <p>Easy peasy highly scalable REactJS & React Native Forms! </p>
-                </div>
-                <FiChevronRight size={20}/>
-            </a>
-            <a href="teste">
-                <img
-                    src="https://avatars1.githubusercontent.com/u/55980259?s=460&u=14290cea59e995dc4544857f4f6e698c9bcc4ec5&v=4"
-                    alt="Gustavo Voigt"
-                />
-                <div>
-                    <strong>rocketseat/unform</strong>
-                    <p>Easy peasy highly scalable REactJS & React Native Forms! </p>
-                </div>
-                <FiChevronRight size={20}/>
-            </a>
+            {repositories.map(repository => (
+                <a key={repository.full_name} href="teste">
+                    <img
+                        src={repository.owner.avatar_url}
+                        alt={repository.owner.login}
+                    />
+                    <div>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            ))}
         </Repositories>
     </>)
 }
