32e72f9f6359f8da14e3ee34babb15032508b5a4
Gustavo Voigt <voigt.gustavoh@gmail.com>
2020-08-02 11:14:06 -0300

    Local Storage for the repositories

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import LogoImg from '../../assets/Logo.svg';
 import api from '../../services/api';
@@ -20,7 +20,19 @@ const Dashboard: React.FC = () => {
 
     const [newRepo, setNewRepo] = useState('');
     const [inputError, setInputError] = useState('');
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories');
+
+        if (storagedRepositories) {
+            return JSON.parse(storagedRepositories);
+        } else {
+            return [];
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories));
+    }, [repositories]);
 
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault();
@@ -56,8 +68,8 @@ const Dashboard: React.FC = () => {
             <button type="submit" >Pesquisar</button>
         </Form>
 
-        { inputError && <Error>{inputError}</Error>}
-        
+        {inputError && <Error>{inputError}</Error>}
+
         <Repositories>
             {repositories.map(repository => (
                 <a key={repository.full_name} href="teste">
